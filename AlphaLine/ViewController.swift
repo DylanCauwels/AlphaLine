f8502ee9bb27d1a3e79f000da2dd6149d0b781f1
Dylan Cauwels <dm.cauwels@verizon.net>
2020-02-05 14:56:23 -0600

    Initial Commit

---
+++ AlphaLine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlphaLine
+//
+//  Created by Dylan Cauwels on 2/5/20.
+//  Copyright © 2020 Group 6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
